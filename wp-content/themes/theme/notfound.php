ae8bd34009a4a5685527485bf8cdf2dc1272397d
Steffen Becker <nebelschwade@gmail.com>
2012-04-09 17:54:04 +0200

    classes are now lowercase and dash-separated

--- wp-content/themes/theme/notfound.php
+++ wp-content/themes/theme/notfound.php
@@ -1,4 +1,4 @@
-<article class="post notFound">
+<article class="post notfound">
   <h1>Ein Fehler ist aufgetreten!</h1>
   <section class="article-content">
     <p>Leider wurde an dieser Stelle nicht der gew&uuml;nschte Inhalt gefunden.</p>
