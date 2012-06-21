ac451e04e13f81f140e160939f14d820685fab88
Steffen Becker <nebelschwade@gmail.com>
2012-06-21 14:17:19 +0200

    comments

--- wp-content/themes/theme/comments.php
+++ wp-content/themes/theme/comments.php
@@ -9,8 +9,8 @@
 
   <h2><?php comments_number('Keine Kommentare', 'Ein Kommentar', '% Kommentare'); ?> zu &raquo;<?php the_title(); ?>&laquo;</h2>
   <?php if (have_comments()) : ?>
-  <ol>
-    <?php wp_list_comments('type=comment&callback=custom_comments'); ?>
+  <ol class="comment-list">
+    <?php wp_list_comments(array('callback' => 'custom_comments')); ?>
   </ol>
 
   <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
