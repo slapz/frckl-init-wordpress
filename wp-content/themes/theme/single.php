de1904be628b047c0a6b3f929936b535967f0740
Steffen Becker <nebelschwade@gmail.com>
2012-07-05 18:31:09 +0200

    minor fix in single - next pages etc. version 1.0

--- wp-content/themes/theme/single.php
+++ wp-content/themes/theme/single.php
@@ -9,6 +9,11 @@
     <h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
     <?php if (has_post_thumbnail()) the_post_thumbnail(); ?>
     <?php the_content(); ?>
+    <?php wp_link_pages(array(
+      'before' => '<p><strong>Weiter zu Seite:</strong> ',
+      'after' => '</p>',
+      'next_or_number' => 'number'
+    )); ?>
   </article>
 
   <?php endwhile; else : ?>
