858771b25adc3017674a73714aa02f84e53478b1
Steffen Becker <webgefrickel@gmail.com>
2013-05-15 10:23:26 +0200

    using main instead of main-div

--- wp-content/themes/theme/wrapper.php
+++ wp-content/themes/theme/wrapper.php
@@ -1,10 +1,10 @@
 <?php get_header(custom_template_base()); ?>
 
-<section class="content" role="main">
+<main class="content" role="main">
 
 <?php include custom_template_path(); ?>
 
-</section>
+</main>
 
 <?php get_sidebar(custom_template_base()); ?>
 <?php get_footer(custom_template_base()); ?>
