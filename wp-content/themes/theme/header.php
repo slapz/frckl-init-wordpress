0d4c1807a4f5e341c97a2a28ae60c4055d6fbc8e
Steffen Becker <webgefrickel@gmail.com>
2013-09-06 16:50:59 +0200

    CC and IE-edge

--- wp-content/themes/theme/header.php
+++ wp-content/themes/theme/header.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <!--[if lt IE 8]> <html class="no-js lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]> <html class="no-js ie8" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+<!--[if IE 9]> <html class="no-js ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
   <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
+
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/main.css" />
-  <script src="<?php echo get_template_directory_uri(); ?>/js/libs/modernizr.min.js"></script>
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+  <script src="<?php echo get_template_directory_uri(); ?>/js/libs/modernizr.min.js"></script>
   <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
   <div class="container">
 
